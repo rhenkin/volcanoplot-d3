abae7c8518545b2c914d0140dfa2720b54a97683
Rafael Henkin <rafael.henkin@gmail.com>
2022-05-11 15:03:11 +0100

    Changes

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import VolcanoPlot from "../src/";
+import { VolcanoPlot } from "../src/index.jsx";
 
 const testData = [
     {"logFC": -5, "logP": 0},
